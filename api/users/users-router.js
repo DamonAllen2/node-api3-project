eed4e8b9e4f9d14e4db800bc8e032a48db20c4c2
Damon Allen <damon.nicholas.allen@live.com>
2023-12-23 13:51:23 -0500

    all working as intended

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validateUserId, validateUser } = require('../middleware/middleware')
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware')
 const User = require('./users-model')
 const Post = require(`../posts/posts-model`)
 // You will need `users-model.js` and `posts-model.js` both
@@ -38,7 +38,7 @@ router.post('/', validateUser, (req, res, next) => {
 router.put('/:id', validateUserId, validateUser, (req, res) => {
   const id = req.params.id;
   const username = req.name;
-  User.update({ id: id, name: username})
+  User.update(id,{ name: username})
   .then(response => {
     res.status(200).json(response);
   })
@@ -47,18 +47,41 @@ router.put('/:id', validateUserId, validateUser, (req, res) => {
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', validateUserId, validateUser, (req, res) => {
+router.delete('/:id', validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
+  const id = req.params.id;
+  const user = req.user;
+  try {
+  await User.remove(id);
+  res.status(200).json(user)
+  } catch (err) {
+    next(err)
+  }
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
+  const id = req.params.id
+  try {
+    const posts = await User.getUserPosts(id);
+    res.status(200).json(posts);
+  } catch (err) {
+    next(err)
+  }
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', validateUserId, validateUser, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
+  const id = req.params.id;
+  const text = req.text;
+  try {
+    const updated = await Post.insert({ user_id: id, text: text })
+    res.status(201).json(updated)
+  } catch (err) {
+    next(err)
+  }
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
